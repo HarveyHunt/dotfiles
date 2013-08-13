a6b43a7bcd394051478239ec4f4374aa13b4d8cb
Harvey Hunt <harveyhuntnexus@gmail.com>
2013-08-13 01:15:03 +0100

    Removed licence info from another project.

--- .i3/scripts/news.py
+++ .i3/scripts/news.py
@@ -1,28 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# This script is a simple wrapper which prefixes each i3status line with custom
-# information. It is a python reimplementation of:
-# http://code.stapelberg.de/git/i3status/tree/contrib/wrapper.pl
-#
-# To use it, ensure your ~/.i3status.conf contains this line:
-#     output_format = "i3bar"
-# in the 'general' section.
-# Then, in your ~/.i3/config, use:
-#     status_command i3status | ~/i3status/contrib/wrapper.py
-# In the 'bar' section.
-#
-# In its current version it will display the cpu frequency governor, but you
-# are free to change it to display whatever you like, see the comment in the
-# source code below.
-#
-# © 2012 Valentin Haenel <valentin.haenel@gmx.de>
-#
-# This program is free software. It comes without any warranty, to the extent
-# permitted by applicable law. You can redistribute it and/or modify it under
-# the terms of the Do What The Fuck You Want To Public License (WTFPL), Version
-# 2, as published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more
-# details.
 import time
 import sys
 import json
